7dbf60aa3d200194a059565797ee5ea10679566a
Mahin <mahinyayaa@gmail.com>
2020-07-23 21:37:51 +0600

    website done

--- script.js
+++ script.js
@@ -1,52 +1,54 @@
-// current phone price event handler
+// subtotal, tax, total
+var subtotal = document.getElementById('subtotal');
+var tax = document.getElementById('tax');
+var total = document.getElementById('total')
 
-const current1 = document.getElementById('phonePrice').innerText;
-const currentNumber1 = parseFloat(current1);
+// minus function
+function minus(price, inputId, priceId) {
+    inputNumber = parseFloat(inputId.value);
+    inputNumber --;
+    inputId.value = inputNumber;
+    priceId.innerText = price * parseFloat(inputId.value);
+    subtotal.innerText = parseFloat(price1.innerText) + parseFloat(price2.innerText);
+    tax.innerText = parseFloat(subtotal.innerText) / 100;
+    total.innerText = parseFloat(subtotal.innerText) + parseFloat(tax.innerText)
+}
 
-// plus button1 event handler
-document.getElementById('plusBtn1').addEventListener('click', function(){
-    const amount1 = document.getElementById('amount1').value;
-    const amountNumber1 = parseFloat(amount1);
-    const totalAmountNumber1 = document.getElementById('amount1').value = amountNumber1 + 1;
+// plus function
+function plus(price, inputId, priceId) {
+    inputNumber = parseFloat(inputId.value);
+    inputNumber ++;
+    inputId.value = inputNumber;
+    priceId.innerText = price * parseFloat(inputId.value);
+    subtotal.innerText = parseFloat(price1.innerText) + parseFloat(price2.innerText);
+    tax.innerText = parseInt(subtotal.innerText/ 100) ;
+    total.innerText = parseFloat(subtotal.innerText) + parseFloat(tax.innerText)
+}
 
-    const totalNumber1 = currentNumber1 * totalAmountNumber1;
-    document.getElementById('phonePrice').innerText = totalNumber1;
-})
+// phone price calculation
+var minusBtn1 = document.getElementById('minusBtn1');
+var plusBtn1 = document.getElementById('plusBtn1');
+var input1 = document.getElementById('input1');
+var price1 = document.getElementById('price1');
 
-// minus button1 event handler
 document.getElementById('minusBtn1').addEventListener('click', function(){
-    const amount1 = document.getElementById('amount1').value;
-    const amountNumber1 = parseFloat(amount1);
-    const totalAmountNumber1 = document.getElementById('amount1').value = amountNumber1 - 1;
-
-    const totalNumber1 = currentNumber1 * totalAmountNumber1;
-    document.getElementById('phonePrice').innerText = totalNumber1;
+    minus(1219, input1, price1)
+})
+document.getElementById('plusBtn1').addEventListener('click', function(){
+    plus(1219, input1, price1)
 })
 
-// current case price event handler
+// case price calculation
+var minusBtn2 = document.getElementById('minusBtn2');
+var plusBtn2 = document.getElementById('plusBtn2');
+var input2 = document.getElementById('input2');
+var price2 = document.getElementById('price2');
 
-const current2 = document.getElementById('casePrice').innerText;
-const currentNumber2 = parseFloat(current2);
-
-// plus button event handler
-document.getElementById('plusBtn2').addEventListener('click', function(){
-    const amount2 = document.getElementById('amount2').value;
-    const amountNumber2 = parseFloat(amount2);
-    const totalAmountNumber2 = document.getElementById('amount2').value = amountNumber2 + 1;
-
-    const totalNumber2 = currentNumber2 * totalAmountNumber2;
-    document.getElementById('casePrice').innerText = totalNumber2;
-})
-
-// minus button event handler
 document.getElementById('minusBtn2').addEventListener('click', function(){
-    const amount2 = document.getElementById('amount2').value;
-    const amountNumber2 = parseFloat(amount2);
-    const totalAmountNumber2 = document.getElementById('amount2').value = amountNumber2 - 1;
-
-    const totalNumber2 = currentNumber2 * totalAmountNumber2;
-    document.getElementById('casePrice').innerText = totalNumber2;
+    minus(9, input2, price2)
 })
-
+document.getElementById('plusBtn2').addEventListener('click', function(){
+    plus(9, input2, price2)
+})
 
 
